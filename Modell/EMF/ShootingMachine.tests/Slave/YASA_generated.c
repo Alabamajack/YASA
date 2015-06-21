2ea8953121478b82281e99b5a5e1d4ffcc3afa45
Philipp-E <ph.eidenschink@gmail.com>
2015-06-21 18:49:25 +0200

    - Erstellung der einzelnen Dateien in Funktionen ausgelagert. - Hilfsvariablen hinzugefügt. - Debugausgaben ergänzt.

--- Modell/EMF/ShootingMachine.tests/Slave/YASA_generated.c
+++ Modell/EMF/ShootingMachine.tests/Slave/YASA_generated.c
@@ -1,6 +1,8 @@
 DeclareEvent(RTE_Output_GetValue_Receiver_In_EVENT);
 U8 RTE_Output_GetValue_Receiver_In_SPEICHER[MAX_MESSAGE_LENGHT] = {0};
-inline std_return RTE_Schussanlage_SendMessage_Sender_Out(char *a){
-RTE_Output_GetValue_Receiver_In_SPEICHER = *a;
-SetEvent(, RTE_Output_GetValue_Receiver_In_EVENT);
+
+inline std_return RTE_Schussanlage_SendMessage_Sender_Out(char *a)
+{
+	RTE_Output_GetValue_Receiver_In_SPEICHER = *a;
+	SetEvent(Output_Task, RTE_Output_GetValue_Receiver_In_EVENT);
 }
