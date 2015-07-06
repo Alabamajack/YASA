0702c413079904d40cd935efc33b9a7c090789d9
Litschi <tobias.schwindl@live.de>
2015-07-06 12:50:12 +0200

    Update Schussanlage_SetOrder_Client_Out.c

--- Code/runnable/Schussanlage_SetOrder_Client_Out.c
+++ Code/runnable/Schussanlage_SetOrder_Client_Out.c
@@ -1,4 +1,4 @@
-std_return Schussanlage_SetOrder_Client_Out(uint8_t* val, uint32_t*)
+std_return Schussanlage_SetOrder_Client_Out(uint8_t* val)
 {
 	RTE_SchussMotor_SetOutputValue_OSPort_Out((*val) * 360);
 	return 0;
